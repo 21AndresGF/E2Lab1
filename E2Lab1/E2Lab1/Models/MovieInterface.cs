956126357e076cdc9c2c15ea1339e3cd7c4d9f35
diegosalazar00 <diego97szr@gmail.com>
2020-01-23 12:30:55 -0600

    interfaces

---
+++ E2Lab1/E2Lab1/Models/MovieInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E2Lab1.Models
+{
+    interface MovieInterface
+    {
+        public void Add(Movie newdata);
+
+        public List<Movie> library();
+    }
+}
