292ec9624d822ad25e23706cfe2491b03411cc25
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-01-23 11:12:43 -0600

    conect settings with controller

--- E2Lab1/E2Lab1/Startup.cs
+++ E2Lab1/E2Lab1/Startup.cs
@@ -26,7 +26,7 @@ namespace E2Lab1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSingleton<>;
+            //services.AddSingleton<>;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
